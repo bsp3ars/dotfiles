2b0d39917b9935f87781053a8aec1d4741c89ee7
Brandon Spears <bsp3ars@gmail.com>
2025-07-29 11:41:03 -0400

    Move setup scripts to folder

--- setup/install.sh
+++ setup/install.sh
@@ -2,8 +2,8 @@ case "$OSTYPE" in
   darwin*)  
     source macos.sh ;; 
   linux*)
-    source linux.sh ;;
-  msys*)    
-    source windows.sh ;;
+    source linux.min.sh ;;
+  # msys*)    
+  #   source windows.sh ;;
   *)        echo "Not currently supported: $OSTYPE" ;;
 esac
