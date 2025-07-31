ebef35a1104e6a37004a4e06dfbafa7e7897d5ec
Brandon Spears <bsp3ars@gmail.com>
2025-07-31 02:44:19 +0000

    Add base .gitconfig

--- setup/linux.min.sh
+++ setup/linux.min.sh
@@ -15,6 +15,15 @@ for FILE in ~/dotfiles/bash/.*; do
 	[ -r "$FILE" ] && [ -f "$FILE" ] && ln -sv $FILE ~ 2>/dev/null
 done;
 
+# Symlink .gitconfig based on argument
+if [ $# -eq 0 ]; then
+    ln -sv $START_FOLDER/config/base/.gitconfig ~
+elif [ "$1" == "wsl" ]; then
+    ln -sv $START_FOLDER/config/wsl/.gitconfig ~
+else
+    echo "Invalid argument provided. Current accepted arguments are: (blank), wsl. Skipping .gitconfig symlink..."
+fi
+
 unset START_FOLDER CUSTOM_TIMESTAMP FILE
 
 source ~/.bash_profile
